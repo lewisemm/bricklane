18e628bb5747e3eb64e9cb58fbb2180fa17ea9ba
Lewis Kabui <lewisemm@users.noreply.github.com>
2020-02-03 18:12:13 +0300

    Implement pay via bank transfer feature

--- src/bricklane_platform/models/payment.py
+++ src/bricklane_platform/models/payment.py
@@ -3,6 +3,7 @@ from dateutil.parser import parse
 
 
 from bricklane_platform.models.card import Card
+from bricklane_platform.models.bank import Bank
 from bricklane_platform.config import PAYMENT_FEE_RATE
 
 
@@ -13,8 +14,11 @@ class Payment(object):
     amount = None
     fee = None
     card_id = None
+    source = None
 
-    def __init__(self, data=None):
+    def __init__(self, data=None, source="card"):
+
+        self.source = source
 
         if not data:
             return
@@ -26,10 +30,19 @@ class Payment(object):
         self.fee = total_amount * PAYMENT_FEE_RATE
         self.amount = total_amount - self.fee
 
-        card = Card()
-        card.card_id = int(data["card_id"])
-        card.status = data["card_status"]
-        self.card = card
+        if self.source.lower() == "card":
+            card = Card()
+            card.card_id = int(data["card_id"])
+            card.status = data["card_status"]
+            self.card = card
+        elif self.source.lower() == "bank":
+            bank = Bank()
+            bank.bank_account_id = data["bank_account_id"]
+            bank.processed = True
+            self.bank = bank
 
     def is_successful(self):
-        return self.card.status == "processed"
+        if self.source.lower() == "card":
+            return self.card.status == "processed"
+        elif self.source.lower() == "bank":
+            return self.bank.processed
